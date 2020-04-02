e380eb85164d3eb2235f9c816a46fd44c5a43746
Rannojit <cserannojitsarkar22@gmail.com>
2020-04-02 12:11:27 +0600

    ...

--- themes/ash/404.php
+++ themes/ash/404.php
@@ -6,11 +6,11 @@
   </div>
 </section>
 
-<section class="main-content page-404-wrp">
+<section class="main-content page-404-wrp error-404-wrap">
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <div class="page-404 text-center">
+                <div class="page-404  text-center">
                     <h1>404</h1>
                     <h3>It looks like nothing was found at this location.</h3>
                 </div>
